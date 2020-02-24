b4aeaf0b677b79c88b7644e5bdecdb4e6c73ede1
twhite06 <50536683+twhite06@users.noreply.github.com>
2020-02-24 01:46:48 -0500

    Create stringProcessing.h

---
+++ stringProcessing.h
@@ -0,0 +1,18 @@
+ * stringProcessing.h
+ * COMP15
+ * Spring 2019
+ *
+ *
+ * Author: Tim White 
+ * Date: 11/15/2019
+
+#ifndef _STRING_PROCESSING_H_
+#define _STRING_PROCESSING_H_
+
+#include <string>
+
+using namespace std;
+
+string stripNonAlphaNum(string input);
+
+#endif
